9bafbf64fd2239122f628d62368e3559e6dcda22
SerDavran <serafimntavranis@hotmail.com>
2018-05-05 09:31:48 +0300

    Object and arrays

--- controller.js
+++ controller.js
@@ -1,3 +1,7 @@
 app.controller('myController', function($scope) {
     $scope.name = 'Serafim';
+    // Object
+    $scope.person = {firstName:'Serafim', lastName:'Ntavranis'};
+    // Arrays
+    $scope.myArray = [1, 3, 6, 7]; 
 });
