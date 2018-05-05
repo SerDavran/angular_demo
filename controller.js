f22e3330d3ba0b02967b6bca5050116e8061a3a2
SerDavran <serafimntavranis@hotmail.com>
2018-05-05 09:55:36 +0300

    Array repeat

--- controller.js
+++ controller.js
@@ -1,5 +1,7 @@
 app.controller('myController', function($scope) {
     $scope.name = 'Serafim';
+    $scope.quantity = 0;
+    $scope.cost = 0;
     // Object
     $scope.person = {firstName:'Serafim', lastName:'Ntavranis'};
     // Arrays
