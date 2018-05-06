dca2959b8a8693632615138788c52189cf3a3b66
SerDavran <serafimntavranis@hotmail.com>
2018-05-06 15:10:00 +0300

    Function on click

--- controller.js
+++ controller.js
@@ -1,9 +1,18 @@
 app.controller('myController', function($scope) {
     $scope.name = 'Serafim';
+    $scope.startingVar = 'First var name';
     $scope.quantity = 0;
     $scope.cost = 0;
     // Object
     $scope.person = {firstName:'Serafim', lastName:'Ntavranis'};
     // Arrays
     $scope.myArray = [1, 3, 6, 7]; 
+    // Function
+    $scope.changeName = function(e){
+        if( $scope.startingVar != 'Second var name' ){
+            $scope.startingVar = 'Second var name';
+        }else{
+            $scope.startingVar = 'First var name';
+        }
+    }
 });
