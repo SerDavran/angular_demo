cf1464598a088ca55b18e10f1a8f13509f74f837
SerDavran <serafimntavranis@hotmail.com>
2018-05-09 22:52:26 +0300

    http intervals and select

--- controller.js
+++ controller.js
@@ -1,10 +1,17 @@
-app.controller('myController', function($scope) {
+// http & interval service
+app.controller('myController', function($scope, $http, $interval) {
+    $scope.time = new Date().toLocaleTimeString();
+    $interval(function(){
+        $scope.time = new Date().toLocaleTimeString();
+    }, 1000);
     $scope.name = 'Serafim';
     $scope.startingVar = 'First var name';
     $scope.quantity = 0;
     $scope.cost = 0;
     // Object
-    $scope.person = {firstName:'Serafim', lastName:'Ntavranis'};
+    $scope.person = [{firstName:'Serafim', lastName:'Ntavranis'},
+                    {firstName:'Serafim2', lastName:'Ntavranis2'},
+                    {firstName:'Serafim3', lastName:'Ntavranis3'}];
     // Arrays
     $scope.myArray = [1, 3, 4, 6, 7, 24, 8, 46]; 
     $scope.names = [
@@ -19,11 +26,18 @@ app.controller('myController', function($scope) {
         'Kai'
     ];
     // Function
-    $scope.changeName = function(e){
+    $scope.changeName = function(){
         if( $scope.startingVar != 'Second var name' ){
             $scope.startingVar = 'Second var name';
         }else{
             $scope.startingVar = 'First var name';
         }
+    }
+
+    // http
+    $scope.getResponce = function(){
+        $http.get("response.html").then(function(response){
+            $scope.response_val = response.data;
+        });
     }
 });
